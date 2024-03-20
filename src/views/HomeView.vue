60dd53fcee0bd34299b6e42fee1fbdfcea60558b
Rain <rain.tuur@gmail.com>
2024-03-20 15:10:10 +0200

    FEB-4 complete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
 
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>See on meie äge pank</h1>
+      </div>
+      <div class="col">
+        <img src="../assets/logo.png" class="img-fluid">
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
