2dfa4d07b92b4bcaf6f1054a349daf1197623b21
Rain <rain.tuur@gmail.com>
2024-03-20 16:28:49 +0200

    FEB-5 complete

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import ErrorView from "@/views/ErrorView.vue";
 
 const routes = [
   {
     path: '/',
     name: 'homeRoute',
     component: HomeView
+  },
+  {
+    path: '/error',
+    name: 'errorRoute',
+    component: ErrorView
   }
 ]
 
