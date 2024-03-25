ac726e61fc3e0f1001904f1861f6c0c56403b96a
Rain <rain.tuur@gmail.com>
2024-03-25 15:01:30 +0200

    FEB-10 WIP

---
+++ src/components/location/LocationsTable.vue
@@ -0,0 +1,68 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+    <tr>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>Tallinn</td>
+      <td>Mustamäe Prisma</td>
+      <td>
+        <p>Sularaha sisse</p>
+        <p>Sularaha välja</p>
+        <p>Välja</p>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'LocationsTable',
+  data(){
+    return {
+      selectedCityId: 0,
+
+      atmLocations: [
+        {
+          locationId: 0,
+          cityName: '',
+          locationName: '',
+          transactionTypes: [
+            {
+              transactionTypeName: ''
+            }
+          ]
+        }
+      ]
+
+
+    }
+  },
+  methods: {
+
+    sendGetAtmLocationsRequest() {
+      this.$http.get("/atm/locations", {
+            params: {
+              cityId: this.selectedCityId
+            }
+          }
+      ).then(response => {
+        const responseJSON = response.data
+      }).catch(error => {
+        const errorResponseJSON = error.response.data
+      })
+    },
+
+
+
+  },
+  beforeMount() {
+
+  }
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -9,44 +9,20 @@
       <div class="col col-2">
         <CitiesDropdown @event-selected-city-change="executeProofOfConceptAlert"/>
       </div>
-
-
       <div class="col col-6">
-        <table class="table table-dark table-hover">
-          <thead>
-          <tr>
-            <th scope="col">Linn</th>
-            <th scope="col">Asukoht</th>
-            <th scope="col">Teenused</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td>Tallinn</td>
-            <td>Mustamäe Prisma</td>
-            <td>
-              <p>Sularaha sisse</p>
-              <p>Sularaha välja</p>
-              <p>Välja</p>
-            </td>
-          </tr>
-          </tbody>
-        </table>
+        <LocationsTable/>
       </div>
-
-
-
-
     </div>
   </div>
 </template>
 
 <script>
 import CitiesDropdown from "@/components/city/CitiesDropdown.vue";
+import LocationsTable from "@/components/location/LocationsTable.vue";
 
 export default {
   name: 'AtmsView',
-  components: {CitiesDropdown},
+  components: {LocationsTable, CitiesDropdown},
   data() {
     return {
       userId: sessionStorage.getItem('userId'),
